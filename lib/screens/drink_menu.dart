6137b945cfde1fa69374f9c4338bc42c8030813a
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2023-11-17 15:17:55 -0300

    Implemented landscape orientation

--- lib/screens/drink_menu.dart
+++ lib/screens/drink_menu.dart
@@ -38,12 +38,17 @@ class DrinkMenu extends StatelessWidget {
               },
               childCount: items.length,
             ),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-              childAspectRatio: 158/194
-            ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount:
+                    MediaQuery.of(context).orientation == Orientation.landscape
+                        ? 3
+                        : 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                childAspectRatio:
+                    MediaQuery.of(context).orientation == Orientation.landscape
+                        ? 1.2
+                        : 158 / 194),
           ),
         ],
       ),
--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -4,7 +4,6 @@ import 'package:panucci_ristorante/components/highlight_item.dart';
 
 class Highlights extends StatelessWidget {
   const Highlights({Key? key}) : super(key: key);
-  final List items = highlights;
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +24,62 @@ class Highlights extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return HighlightItem(
-                  imageURI: items[index]["image"],
-                  itemTitle: items[index]["name"],
-                  itemPrice: items[index]["price"],
-                  itemDescription: items[index]["description"],
-                );
-              },
-              childCount: items.length,
-            ),
-          ),
+          MediaQuery.of(context).orientation == Orientation.landscape
+              ? const LandscapeList()
+              : const PortraitList(),
         ],
+      ),
+    );
+  }
+}
+
+class PortraitList extends StatelessWidget {
+  const PortraitList({super.key});
+
+  final List items = highlights;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        },
+        childCount: items.length,
+      ),
+    );
+  }
+}
+
+class LandscapeList extends StatelessWidget {
+  const LandscapeList({super.key});
+
+  final List items = highlights;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        },
+        childCount: items.length,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 1.2
       ),
     );
   }
